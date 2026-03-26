a90737094c6f015f8cb057e77fc336c66caf34ec
yordan <yordan@gmail.com>
2026-03-26 13:39:07 -0500

    avance semana1

---
+++ ListaSimple/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaSimple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Lista l = new Lista();
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            l.insertar(int.Parse(textNumero.Text));
+            l.mostrar(listBox1);
+            textNumero.Clear();
+        }
+    }
+}
